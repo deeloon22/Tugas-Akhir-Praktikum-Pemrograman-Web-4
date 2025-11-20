86a9484394346191b6ae1446892e22992f85c962
Delon <deeloon0@gmail.com>
2025-11-20 09:13:34 +0700

    Commit Kedua

--- delete_contact.php
+++ delete_contact.php
@@ -1,22 +1,16 @@
 <?php
-// delete_contact.php - Proses hapus kontak
 require_once 'config.php';
 
-// [PENTING] Cek login dulu
 requireLogin();
 
-// Ambil ID kontak
 $id = $_GET['id'] ?? '';
 
-// Cek apakah kontak ada
 $contact = getContactById($id);
 
 if ($contact) {
-    // Hapus kontak
     deleteContact($id);
 }
 
-// Redirect ke index
 header('Location: index.php');
 exit;
 ?>
